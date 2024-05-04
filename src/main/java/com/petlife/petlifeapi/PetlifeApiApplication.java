8a88adb286e6042a7a5d05370b1c9cf0f067a46d
Ari <ari.aguiar@sefin.fortaleza.ce.gov.br>
2024-05-04 16:31:56 -0300

    Iniciando projeto.

---
+++ src/main/java/com/petlife/petlifeapi/PetlifeApiApplication.java
@@ -0,0 +1,13 @@
+package com.petlife.petlifeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetlifeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetlifeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petlife/petlifeapi/PetlifeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petlife.petlifeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetlifeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
